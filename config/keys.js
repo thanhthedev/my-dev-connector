c39da674e558000d565640035ec28aca39dfdc56
thanhthedev <huythanhdao@gmail.com>
2019-01-18 15:05:01 +1000

    added routes and mlabs

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://admin:react123@ds159164.mlab.com:59164/my-developer-connector'
+    
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
+
+const app = express()
+
+
+// DB config
+const db = require('./config/keys').mongoURI
+
+//Connect to Mongoose
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('Heya boi')
+})
+
+//Use routes
+app.use('/api/users', users)
+app.use('/api/posts', posts)
+app.use('/api/profile', profile)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
